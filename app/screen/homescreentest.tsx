8dad5562f06cad419dbd279746a145673e1436f8
isse.kai <micadario0926@gmail.com>
2025-09-15 16:51:46 +0800

    removed unnecessaries

--- app/screen/homescreentest.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'dripsy'
-
-export default function HomeScreen() {
-  return (
-    <View sx={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text sx={{ fontSize: 22, fontFamily: 'Poppins-Bold' }}>🏠 Home Screen</Text>
-    </View>
-  )
-}
